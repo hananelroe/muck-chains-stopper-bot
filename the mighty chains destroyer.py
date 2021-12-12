1d88b3650b8a1cbce7fbe85c39d5ff6bb6bd054d
hananelroe <91851891+hananelroe@users.noreply.github.com>
2021-12-12 14:41:31 +0200

    Update the mighty chains destroyer.py

--- the mighty chains destroyer.py
+++ the mighty chains destroyer.py
@@ -10,10 +10,10 @@ Muck_list = ["muck", "muck.", "muck!", "muck?",
              "m u c k", "m\*ck", "kcum"]
 
 # initialize with appropriate values
-client_id = "bfmkcXi2IZsWfmP9c0OtoA"
-client_secret = "RDQq9Fb802LT19-sLZWVGX49wC0CRg"
+client_id = ""
+client_secret = ""
 username = "DaniDevChainBreaker"
-password = "BreakDaMuckChains"
+password = ""
 user_agent = "u/hananelroe's comment chains breaker bot"
 comment_content = "#**SHUT**\n___\n ^(I'm just a simple bot that wants to stop muck chains, [here is my source code](https://github.com/hananelroe/muck-chains-stopper-bot))\n\n ^(oh and if you're a real boner - upvote this comment. it helps my karma.)"
 
