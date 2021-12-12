be92759d7df45d47d664980bc92f9ec668e8f0f6
hananelroe <91851891+hananelroe@users.noreply.github.com>
2021-12-12 18:43:42 +0200

    Update the mighty chains destroyer.py

--- the mighty chains destroyer.py
+++ the mighty chains destroyer.py
@@ -1,21 +1,32 @@
+#!/data/data/com.termux/files/usr/bin/python3
 # this bot was made by u/hananelroe on reddit
 import thefuzz.fuzz as fuzz
 from thefuzz import *
 import praw
+import unicodedata
+
+
+def noglyph(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                   if unicodedata.category(c) != 'Mn')
+
 
 print(praw.__version__)
 
-Muck_list = ["muck", "muck.", "muck!", "muck?",
-             "mück", "mück.", "mück!", "mück?",
-             "m u c k", "m\*ck", "kcum"]
+# Muck_list = ["#**SHUT**\n___\n ^(I'm just a simple bot that wants to stop muck chains, [here is my source code](https://github.com/hananelroe/muck-chains-stopper-bot))\n\n ^(oh and if you're a real boner - upvote this comment. it helps my karma.)"]
+Muck_list = ["muck", "muck.", "muck!", "muck?", "mֳ¼ck", "mֳ¼ck.", "mֳ¼ck!", "mukc", "mֳ¼ck?", "m u c k", "m\*ck",
+             "kcum", "׀¼uck", "mick", "much", "mcuk"]
+Blocked_users = []
 
 # initialize with appropriate values
 client_id = ""
 client_secret = ""
-username = "DaniDevChainBreaker"
+username = ""
 password = ""
-user_agent = "u/hananelroe's comment chains breaker bot"
-comment_content = "#**SHUT**\n___\n ^(I'm just a simple bot that wants to stop muck chains, [here is my source code](https://github.com/hananelroe/muck-chains-stopper-bot))\n\n ^(oh and if you're a real boner - upvote this comment. it helps my karma.)"
+user_agent = "u/hananelroe's and u/norecap_bot's comment chains breaker bot"
+comment_content = "#**SHUT**\n___\n ^(I'm just a simple bot that wants to stop muck chains, [here is my source code](https://github.com/hananelroe/muck-chains-stopper-bot))\n\n ^(oh and if you're a real boner - upvote this comment. it helps my karma.)\n^(also here is your IP: 127.0.0.1 LOL)\n^(I'm a collaboration between u/DaniDevChainBreaker and u/norecap_bot)"
+why = "WHY?\n\n^(this is a port of the dani dev chain breaker! we are breaking muck, much, mukc and etc chains)\n\n^(if you're a real boner - upvote this comment. it helps my karma.)"
+com = ""
 
 while True:
     # creating an authorized reddit instance
@@ -30,15 +41,23 @@ while True:
 
     try:
         for comment in subreddit.stream.comments(skip_existing=True):
-            print(comment.body)
-            # check if the comment is above 74% muck: (allows 1 wrong letter in a 4 letters word)
-            for item in Muck_list:
-                if fuzz.ratio(comment.body.lower(), item) > 74:
-                    comment.reply(comment_content)
-                    break  # exit Muck_list loop
+            com = noglyph("".join(dict.fromkeys(comment.body.lower())))
+            print("\u001b[35;1m" + comment.body + "\u001b[34;1m\t" + com + " \u001b[0m" + str(fuzz.ratio(com, "muck")))
+            print("\u001b[36;1m" + str(comment.author) + "\u001b[0m")
+
+            if comment.parent().author.name == username and comment.body.lower() == "bad bot":
+                comment.reply(why)
+                break
+            else:
+                for item in Muck_list:
+                    if fuzz.ratio(com, item) > 74:
+                        for user in Blocked_users:
+                            if str(comment.author) == user:
+                                comment.reply(comment_content)
+                                break
     except KeyboardInterrupt:  # Ctrl-C - stop
         print("Bye!")
-        quit()
+        break
     except Exception as error:  # Any exception
         print(f"Error: {error}")
         print("Trying to restart...")
