f013c85e2e96fa5921fd21fe8723e32ac9eedac6
hananelroe <91851891+hananelroe@users.noreply.github.com>
2021-12-13 20:26:29 +0200

    sensitive information was moved to a seperate file

--- the mighty chains destroyer.py
+++ the mighty chains destroyer.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 # this bot was made by u/hananelroe on reddit
-# exception handeling and neating the code: u/n1k0nP on reddit
 import thefuzz.fuzz as fuzz
 import praw
 import unicodedata
+import DA_SECRETS
 
 print("\u001b[31;1m" + str(praw.__version__))
 
 Muck_list = ["muck", "muck.", "muck!", "muck?", "mֳ¼ck", "mֳ¼ck.", "mֳ¼ck!", "mukc", "mֳ¼ck?", "m u c k", "m\*ck",
              "kcum", "׀¼uck", "mick", "much", "mcuk"]
-Blocked_users = ["DaniDevChainBreaker"]
+Blocked_users = ["u/DaniDevChainBreaker", "Hananelroe"]
 
 # initialize with appropriate values
-client_id = ""
-client_secret = ""
-username = "DaniDevChainBreaker"
-password = ""
+client_id = DA_SECRETS.client_id
+client_secret = DA_SECRETS.client_secret
+username = DA_SECRETS.username
+password = DA_SECRETS.password
 user_agent = "u/hananelroe's and u/norecap_bot's comment chains breaker bot"
-comment_content = "#**SHUT**\n___\n ^(I'm just a simple bot that wants to stop muck chains, [here is my source code](https://github.com/hananelroe/muck-chains-stopper-bot))\n\n ^(oh and if you're a real boner - upvote this comment. it helps my karma.)\n^(also here is your IP: 127.0.0.1 LOL)\n^(I'm a collaboration between u/\u200cDaniDevChainBreaker and u/\u200cnorecap_bot)"
+comment_content = "#**SHUT**\n___\n ^(I'm just a simple bot that wants to stop muck chains, [here is my source code](https://github.com/hananelroe/muck-chains-stopper-bot))\n\n ^(oh and if you're a real boner - upvote this comment. it helps my karma.)\n\n^(also here is your IP: 127.0.0.1 LOL)\n\n^(I'm a collaboration between u/DaniDevChainBreaker and u/norecap_bot)"
 why = "WHY?\n\n^(this is a port of the dani dev chain breaker! we are breaking muck, much, mukc and etc chains)\n\n^(if you're a real boner - upvote this comment. it helps my karma.)"
 fixed_comment = ""
+IsBlocked = False
+
 
 class Empty:
     pass
+
 
 def parent(child_comment):
     parent_comment = Empty()
@@ -30,11 +33,13 @@ def parent(child_comment):
     try:
         parent_comment = child_comment.parent()
     finally:
-        return (parent_comment)
+        return parent_comment
+
 
 def noglyph(s):
     return ''.join(c for c in unicodedata.normalize('NFD', s)
                    if unicodedata.category(c) != 'Mn')
+
 
 while True:
     # creating an authorized reddit instance
@@ -45,26 +50,30 @@ while True:
                          user_agent=user_agent)
 
     subreddit = reddit.subreddit("test")
-    print("\033[92monline\n\u001b[0m")
+    print("\033[92monline\u001b[0m")
 
     try:
         for comment in subreddit.stream.comments(skip_existing=True):
-            fixed_comment = noglyph("".join(dict.fromkeys(comment.body.lower()))).replace(" ","").replace("\n","")
-            print("\u001b[35;1m" + comment.body + "\u001b[34;1m\t" + fixed_comment + " \u001b[0m" + str(fuzz.ratio(fixed_comment, "muck")) + "%")
-            print("u/\u001b[36;1m" + str(comment.author) + "\u001b[0m")
+            fixed_comment = noglyph("".join(dict.fromkeys(comment.body.lower())))
+            print("\u001b[35;1m" + comment.body + "\u001b[34;1m\t" + fixed_comment + " \u001b[0m" + str(
+                fuzz.ratio(fixed_comment, "muck")) + "%")
+            print("u/\u001b[36;1m" + str(comment.author) + "\u001b[0m\n")
 
             if parent(comment).author.name == username and comment.body.lower() == "bad bot":
                 comment.reply(why)
                 break
             else:
                 for item in Muck_list:
-                    if fuzz.ratio(fixed_comment, item) > 80:
-                        #for user in Blocked_users: bug fixed!
-                            if str(comment.author) not in Blocked_users:
-                                print("\033[92mMATCH! replying...\u001b[0m")
-                                comment.reply(comment_content)
+                    if fuzz.ratio(fixed_comment, item) > 74:
+                        for user in Blocked_users:
+                            if user == str(comment.author):
+                                IsBlocked = True
+                                print("\u001b[31;1muser is blocked")
                                 break
-            print()
+                        if IsBlocked == False:
+                            print("\033[92mMATCH! replying...\u001b[0m")
+                            comment.reply(comment_content)
+                        break
     except KeyboardInterrupt:  # Ctrl-C - stop
         print("\u001b[31;1mBye!\u001b[0m")
         break
