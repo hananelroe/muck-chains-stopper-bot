fbd6187e97c8524cdec30bab6153ef1e03fceadd
HoseanRC <68903522+HoseanRC@users.noreply.github.com>
2021-12-14 12:49:51 +0330

    IsBlocked -> Enable_Blocking

--- the mighty chains destroyer.py
+++ the mighty chains destroyer.py
@@ -10,6 +10,7 @@ print("\u001b[31;1m" + str(praw.__version__))
 Muck_list = ["muck", "muck.", "muck!", "muck?", "mֳ¼ck", "mֳ¼ck.", "mֳ¼ck!", "mukc", "mֳ¼ck?", "m u c k", "m\*ck",
              "kcum", "׀¼uck", "much", "mcuk"]
 Blocked_users = ["u/DaniDevChainBreaker", "Hananelroe"]
+Enable_Blocking = False
 
 # initialize with appropriate values
 client_id = DA_SECRETS.client_id
@@ -20,7 +21,6 @@ user_agent = "u/hananelroe's and u/norecap_bot's comment chains breaker bot"
 comment_content = "#**SHUT**\n___\n ^(I'm just a simple bot that wants to stop muck chains, [here is my source code](https://github.com/hananelroe/muck-chains-stopper-bot))\n\n ^(oh and if you're a real boner - upvote this comment. it helps my karma.)\n\n^(also here is your IP: 127.0.0.1 LOL)\n\n^(I'm a collaboration between [u/\u200cHananelroe](https://www.reddit.com/u/Hananelore) and [u/\u200cnorecap_bot](https://www.reddit.com/u/norecap_bot))"
 why = "WHY?\n\n^(I'm a collaboration between u/Hananelroe and u/norecap_bot! we are breaking muck, much, mukc and etc chains)\n\n^(if you're a real boner - upvote this comment. it helps my karma.)"
 fixed_comment = ""
-IsBlocked = False
 
 
 class Empty:
@@ -59,7 +59,7 @@ while True:
             fixed_comment = noglyph("".join(dict.fromkeys(comment.body.lower())))
             print("\u001b[35;1m" + comment.body + "\u001b[34;1m\t" + fixed_comment + " \u001b[0m" + str(
                 fuzz.ratio(fixed_comment, "muck")) + "%")
-            if comment.author.name in Blocked_users and IsBlocked:
+            if comment.author.name in Blocked_users and Enable_Blocking:
                 print("u/\u001b[31;1m" + str(comment.author) + "\u001b[92mBLOCKED\u001b[0m")
                 continue
             else:
