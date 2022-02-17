7151259df080ce08d7cac67c8500149cdcd584f1
hananelroe <91851891+hananelroe@users.noreply.github.com>
2022-02-17 16:02:26 +0200

    Update console.py

--- console.py
+++ console.py
@@ -11,26 +11,40 @@ def saveJSON(file, data):
     json.dump(data, file, indent=4)
     file.truncate()  # remove remaining part
 
-def blockUser(user):
+def not_a_func():
+    pass
+
+def blockUser():
+    user = input()
     a = json.load(preferencesFile)
     a["blocked users"].append(str(user).lower())
     saveJSON(preferencesFile, a)
+    print(f"{user} was added to the blacklist")
 
-def unblockUser(user):
+def unblockUser():
+    user = input()
     a = json.load(preferencesFile)
-    a["blocked users"].remove(str(user).lower())
-    saveJSON(preferencesFile, a)
+    try:
+        a["blocked users"].remove(str(user).lower())
+        saveJSON(preferencesFile, a)
+    except:
+        print(f"{color.RED}\"{user}\" wasn't fount in the blacklist{color.END}")
 
 
 commandsMap = {
-        "quit" : ["",                # command output
-                  "quits console"],  # command description
+        "quit": ["",               # command output
+                 "quits console",  # command description
+                 not_a_func],      # command function
         "print mucks": [f"mucks counted today: {muckbot.mucks_Counter}\nmucks counted yesterday: {muckbot.yesterday_Mucks}",
-                        "prints the muck counted today and yesterday"],
-        "block": [f"{color.RED}enter user name(without u/) to block{color.END}",
-                       "blocks a given user"],
-        "unblock": [f"{color.GREEN}enter user name(without u/) to unblock{color.END}",
-                         "unblocks a given user"]
+                        "prints the muck counted today and yesterday",
+                        not_a_func],
+        "block": [f"{color.RED}enter user name to block{color.END}",
+                  "blocks a given user",
+                  blockUser
+                  ],
+        "unblock": [f"{color.GREEN}enter user name to unblock{color.END}",
+                    "unblocks a given user",
+                    unblockUser]
            }
 
 def main(command):
@@ -40,14 +54,4 @@ def main(command):
     else:
         if command in commandsMap:
             print(commandsMap[command][0])
-            if command == list(commandsMap)[2]:  # block user
-                user = input()
-                blockUser(user)
-                print(f"{color.RED}\"{user}\" was added to blacklist{color.END}")
-            elif command == list(commandsMap)[3]:  # unblock user
-                user = input()
-                try:
-                    unblockUser(user)
-                    print(f"{color.GREEN}\"{user}\" was removed from blacklist{color.END}")
-                except:
-                    print(f"{color.RED}\"{user}\" wasn't found inside the blacklist")
+            commandsMap[command][2]()
