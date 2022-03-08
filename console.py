7d2044fcbf36556350d544bfb7815963b165b219
hananelroe <91851891+hananelroe@users.noreply.github.com>
2022-03-08 10:52:15 +0200

    Update console.py

--- console.py
+++ console.py
@@ -4,48 +4,45 @@ import muckbot
 from colors import color
 
 blockedUser = ""
-preferencesFile = open("preferences.json", "r+")
+preferences = None
 
-def saveJSON(file, data):
-    file.seek(0)  # <--- should reset file position to the beginning.
-    json.dump(data, file, indent=4)
-    file.truncate()  # remove remaining part
 
 def not_a_func():
     pass
 
+
 def blockUser():
+    global preferences
     user = input()
-    a = json.load(preferencesFile)
-    a["blocked users"].append(str(user).lower())
-    saveJSON(preferencesFile, a)
+    preferences["blocked users"].append(str(user).lower())
     print(f"{user} was added to the blacklist")
 
+
 def unblockUser():
+    global preferences
     user = input()
-    a = json.load(preferencesFile)
     try:
-        a["blocked users"].remove(str(user).lower())
-        saveJSON(preferencesFile, a)
+        preferences["blocked users"].remove(str(user).lower())
     except:
         print(f"{color.RED}\"{user}\" wasn't fount in the blacklist{color.END}")
 
 
 commandsMap = {
-        "quit": ["",               # command output
-                 "quits console",  # command description
-                 not_a_func],      # command function
-        "print mucks": [f"mucks counted today: {muckbot.mucks_Counter}\nmucks counted yesterday: {muckbot.yesterday_Mucks}",
-                        "prints the muck counted today and yesterday",
-                        not_a_func],
-        "block": [f"{color.RED}enter user name to block{color.END}\n(without u/)",
-                  "blocks a given user",
-                  blockUser
-                  ],
-        "unblock": [f"{color.GREEN}enter user name to unblock{color.END}\n(without u/)",
-                    "unblocks a given user",
-                    unblockUser]
-           }
+    "quit console": ["",  # command output
+                     "quits console",  # command description
+                     not_a_func],  # command function
+    "print mucks": [f"mucks counted today: {muckbot.mucks_Counter}\nmucks counted yesterday: {muckbot.yesterday_Mucks}",
+                    "prints the muck counted today and yesterday",
+                    not_a_func],
+    "block": [f"{color.RED}enter user name to block{color.END}\n(without u/)",
+              "blocks a given user",
+              blockUser
+              ],
+    "unblock": [f"{color.GREEN}enter user name to unblock{color.END}\n(without u/)",
+                "unblocks a given user",
+                unblockUser]
+}
+
 
 def main(command):
     if command == "help":
